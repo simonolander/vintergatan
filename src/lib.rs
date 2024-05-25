35c59d91b656cb1d1e5a11fe3c6c5413879dc4da
Simon Olander Sahlén <simon.olander@gmail.com>
2024-05-25 19:08:38 +0200

    Greeting

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,15 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
--- www/index.js
+++ www/index.js
@@ -1,1 +1,4 @@
+import {greet} from '../pkg';
 import("../pkg/index.js").catch(console.error);
+
+greet("World")
