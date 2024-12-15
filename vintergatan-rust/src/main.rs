30de9c5d59765513b196b0edabde96c350bb35c7
Simon Olander Sahlén <simon.olander@gmail.com>
2024-12-15 18:01:36 +0100

    Fix rectangle

--- vintergatan-rust/src/main.rs
+++ vintergatan-rust/src/main.rs
@@ -2,7 +2,7 @@ use vintergatan_rust::model::galaxy::Galaxy;
 use vintergatan_rust::model::universe::Universe;
 
 fn main() {
-    let width = 5;
+    let width = 10;
     let height = width;
     let universe = Universe::generate(width, height);
     println!("{}", universe);
