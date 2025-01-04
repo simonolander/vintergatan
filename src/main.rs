dcad1ae855fb75e248fb0de2ebdeb03f462eb1da
Simon Olander Sahlén <simon.olander@gmail.com>
2025-01-04 14:56:25 +0100

    Add switch for running generation in console

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
+use std::thread::sleep;
+use std::time::Duration;
 use crate::app::App;
+use crate::model::universe::Universe;
 
 mod app;
 mod model;
 
+const CONSOLE: bool = false;
+
 fn main() {
-    console_error_panic_hook::set_once();
-    App::new().expect("Failed to create application");
+    if CONSOLE {
+       loop {
+           let universe = Universe::generate(10, 10);
+           println!("{universe}");
+           println!("{}", universe.get_score());
+           println!();
+           sleep(Duration::from_millis(1000));
+       }
+    }
+    else {
+        console_error_panic_hook::set_once();
+        App::new().expect("Failed to create application");
+    }
 }
