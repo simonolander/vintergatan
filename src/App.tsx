e2384cf02499686de7db5a1659a6259de57f53ca
Simon Olander Sahlén <simon.olander@gmail.com>
2026-03-25 21:18:46 +0100

    Reduce the size of the galaxy center dots

--- src/App.tsx
+++ src/App.tsx
@@ -366,7 +366,7 @@ function Board({ view, onToggle }: BoardProps) {
           const cy =
             WALL_SIZE / 2.0 +
             ((WALL_SIZE + CELL_SIZE) / 2.0) * (center.position.row + 1);
-          const r = CELL_SIZE / 2.5 - WALL_SIZE;
+          const r = CELL_SIZE / 3.0 - WALL_SIZE;
 
           const cut = view.error?.cut_centers.some(
             (p) =>
